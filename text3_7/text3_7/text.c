eb60e4e7b5eb8504801343a2f78030b89d2d0a50
Y-rwen <yanruiweng@outlook.com>
2024-08-05 12:49:02 +0800

    3_7

---
+++ text3_7/text3_7/text.c
@@ -0,0 +1,86 @@
+#define   _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//int main()
+//{
+//	int arr1[] = {1,3,5,7,9};
+//	int arr2[] = {2,4,6,8,0};
+//	int i = 0;
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		int tmp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", arr1[i]);
+//	}
+//	printf("\n");
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", arr2[i]);
+//	}
+//	printf("\n");
+//
+//	return 0;
+//}
+
+
+//void init(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//		printf("%d", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//void print(int * arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//void reverse(int* arr, int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr ,sz);
+//	reverse(arr, sz);
+//	init(arr, sz);
+//
+//	return 0;
+//}
